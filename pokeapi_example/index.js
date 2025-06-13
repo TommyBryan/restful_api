87a391337909ceb8ed4406dbf1ff3e9d98d8a089
TommyBryan <tommybryan0713@gmail.com>
2025-06-13 17:32:04 -0400

    Made fixes in code

--- pokeapi_example/index.js
+++ pokeapi_example/index.js
@@ -1,20 +1,21 @@
 #!/usr/bin/env node
-async function fetchData() {
+async function fetchData(){
 
     try{
 
         const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
-        const response = await fetch('https://pokeapi.co/api/v2/pokemon/${pokemonName}');
-    
-        if(!response.ok){
-            throw new Error("Could not fetch data");
-        }
-        const data = await response.json();
-        const pokemonsprite = data.sprites.front_default;
-        const pokemonImage = document.getElementById("pokemonImage");
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
 
-        pokemonImage.src = pokemonsprite;
-        pokemonImage.style.display = "bloc"
+        if(!response.ok){
+            throw new Error("Could not fetch resource");
+        }
+
+        const data = await response.json();
+        const pokemonSprite = data.sprites.front_default;
+        const imgElement = document.getElementById("pokemonSprite");
+
+        imgElement.src = pokemonSprite;
+        imgElement.style.display = "block";
     }
     catch(error){
         console.error(error);
